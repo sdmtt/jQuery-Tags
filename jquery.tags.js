e82c5313b805fe0412bc6810b2b43760e71cf70a
Santiago Dimattia <santiagodimattia@gmail.com>
2012-11-11 00:52:31 -0300

    Fixes #1 (Thanks to ReNDeZOwnler)

--- jquery.tags.js
+++ jquery.tags.js
@@ -17,38 +17,38 @@
 			tagRemoved:  function() { }
 		};
 		var settings = $.extend(defaults, options);
-				
+
 		$(this).each(function(){
 			var e = $(this);
 			var instance = {
-				
+
 				textfield: e,
 				taglist: { },
-				
+
 				init: function(){
 					var that = this;
 					var e = this.textfield;
-					
+
 					if(!e.is('input[type=text]'))
 					{
 						return;
 					}
-					
+
 					// Wrap the input field and create the tag list
 					e.wrap('<div class="tag-manager" />');
 					e.before('<ul class="tag-list"></ul>');
-					
+
 					// Replace the text field with a hidden one
 					e.before('<input type="hidden" name="' + e.attr('name') + '" />');
 					e.removeAttr('name');
-					
+
 					that.add_tag();
-					
+
 					// Bind the container so it focus the text field when you click on it
 					e.parent().bind('click', function(){
 						e.focus();
 					});
-					
+
 					// Bind the input field
 					e.bind('blur', function(){
 						that.add_tag();
@@ -60,7 +60,7 @@
 						}
 					});
 				},
-				
+
 				// Update the hidden field
 				updateHiddenField: function(){
 					var string = '';
@@ -70,48 +70,48 @@
 					}
 					this.textfield.parent().children('input[type=hidden]').val(string.substring(1));
 				},
-				
+
 				// Add one or more tags
 				add_tag: function() {
 					var that = this;
 					var e = this.textfield;
-					
+
 					if(e.val() == '')
 					{
 						return;
 					}
-					
+
 					// Separe the tags by comma
 					tags = e.val().split(settings.separator);
-					
+
 					// For each tag
 					for(i in tags)
 					{
 						// Trim
 						tag = tags[i].replace(/^\s+|\s+$/g, '');
-						
+
 						// Add the tag only if it isn't on the list already
 						if(that.taglist[tag.toLowerCase()] === undefined)
 						{
 							// Add the tag on the list
 							e.parent().children('ul.tag-list').append('<li data-name="' + tag + '">' + tag + ' <a class="tag-remove-link">X</a></li>');
-							
+
 							// Add the tag on the array
 							that.taglist[tag.toLowerCase()] = tag;
-							
+
 							// Delete the tag when the link is clicked!
 							e.parent().find('li[data-name=' + tag + '] a').unbind().click(function(){
-								tagname = $(this).parent().data('name').toLowerCase();
-															
+								tagname = $(this).parent().data('name').toString().toLowerCase();
+
 								// Delete the key
 								delete that.taglist[tagname];
-								
+
 								// Update the hidden input
 								that.updateHiddenField();
-								
+
 								// Remove the li
 								$(this).parent().remove();
-								
+
 								// Callback
 								settings.tagRemoved(tag, e);
 							});
@@ -120,14 +120,14 @@
 						// Callback
 						settings.tagAdded(tag, e);
 					}
-					
+
 					// Update the hidden input
 					that.updateHiddenField();
-				
+
 					e.val('');
-				}				
+				}
 			};
-			
+
 			instance.init();
 		});
 	}
